4974a8049131113b1b450ba0d85d641c368823e5
DGRod <DGRodriguezNY@gmail.com>
2022-11-28 13:04:09 -0500

    Added Player() object, ai() method, and first 2 AI difficulties

--- battleship.py
+++ battleship.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 class Point:
@@ -30,7 +31,26 @@ class Point:
             for letter in sliced_letters:
                 result.append(letter + self.ycoord)
             return result
-            
+    
+    def neighbors(self):
+        numbers = [str(x) for x in range(0, 10)]
+        abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        neighbors = []
+        
+        if self.ycoord != "9":
+            neighbors.append(self.xcoord + numbers[numbers.index(self.ycoord) + 1])
+
+        if self.ycoord != "0":
+            neighbors.append(self.xcoord + numbers[numbers.index(self.ycoord) - 1])
+        
+        if self.xcoord != "J":
+            neighbors.append(abc[abc.index(self.xcoord) + 1] + self.ycoord) 
+        
+        if self.xcoord != "A":
+            neighbors.append(abc[abc.index(self.xcoord) - 1] + self.ycoord)
+        
+        return neighbors
+
 
     def hit(self):
         self.value = "X"
@@ -70,7 +90,7 @@ class Ship:
 
     
     def __repr__(self):
-        return self.name + "has " + str(self.health) + " health."
+        return self.name + " has " + str(self.health) + " health."
 
     def damage(self):
         self.health += -1
@@ -80,9 +100,9 @@ class Ship:
         if self.health == 0:
             self.sunk = True
             if self.team == "Enemy":
-                return "You sunk the enemy " + self.name + "!"
+                return print("You sunk the enemy " + self.name + "!")
             else:
-                return "Your " + self.name + " has been sunk!"
+                return print("Your " + self.name + " has been sunk!")
 
         
     
@@ -91,6 +111,9 @@ class Grid:
 
     def __init__(self, name):
         self.name = name
+        self.all_ships = []
+        self.total_health = 0
+        
 
     #Defining the Points:
         numbers = [str(x) for x in range(0, 10)]
@@ -147,7 +170,10 @@ class Grid:
         bad_positions = []
 
         for point in ship.position:
-            self.points_dict[point].make_ship(ship.name[0])
+            if ship.team == "Player":
+                self.points_dict[point].make_ship(ship.name[0])
+            if ship.team == "Enemy":
+                self.points_dict[point].make_ship("-")
             taken_points.append(point)
 
         for point in taken_points:
@@ -163,6 +189,9 @@ class Grid:
         for position in bad_positions:
             if position in self.all_positions_on_grid:
                 self.all_positions_on_grid.remove(position)
+
+        self.all_ships.append(ship)
+        self.total_health += ship.health
 
 
     def print_grid(self):
@@ -182,19 +211,168 @@ class Grid:
             symbolJ = (self.points_dict[row[9]]).value
             print("{} {} {} {} {} {} {} {} {} {} {}".format(num, symbolA, symbolB, symbolC, symbolD, symbolE, symbolF, symbolG, symbolH, symbolI, symbolJ))
 
+    
+    def reveal_ships(self):
+        for ship in self.all_ships:
+            for point in ship.position:
+                self.points_dict[point].value = '#'
+
+
+
+        
+
+
+class Player:
+
+    def __init__(self, team, name, grid, enemy_grid):
+        self.team = team
+        self.name = name
+        self.grid = grid
+        self.enemy_grid = enemy_grid
+        self.ships = self.grid.all_ships
+        self.target_points = self.enemy_grid.points_list
+        self.previous_target = "A0"
+        self.hit_point = "  "
+        self.hit_list = []
+
+
+    def ai(self, difficulty=1):
+
+        if difficulty == 2:
+            target = random.choice(self.target_points)
+            likely_points = []
+
+            numbers = [str(x) for x in range(0, 10)]
+            abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+            if self.hit_point != "  ":
+                if len(self.hit_list) >= 2:
+
+                    for point in self.hit_list:
+                        other_points = []
+                        for pt in self.hit_list:
+                            if pt != point:
+                                other_points.append(pt)
+                        for other_point in other_points:
+                            if other_point in self.enemy_grid.points_dict[point].neighbors():
+                                if point[0] == other_point[0]:
+                                    if point[1] < other_point[1]: 
+                                        if point[1] != "0":
+                                            likely_points.append(point[0] + str(int(point[1]) - 1))
+                                        if other_point[1] != "9":
+                                            likely_points.append(other_point[0] + str(int(other_point[1]) + 1))
+                                    if point[1] > other_point[1]:
+                                        if point[1] != "9":
+                                            likely_points.append(point[0] + str(int(point[1]) + 1))
+                                        if other_point[1] != "0":
+                                            likely_points.append(other_point[0] + str(int(other_point[1]) - 1))
+                                if point[1] == other_point[1]:
+                                    if abc.index(point[0]) < abc.index(other_point[0]):
+                                        if abc.index(point[0]) != 0:
+                                            likely_points.append(abc[abc.index(point[0]) - 1] + point[1])
+                                        if abc.index(other_point[0]) != 9:
+                                            likely_points.append(abc[abc.index(other_point[0]) + 1] + other_point[1])
+                                    if abc.index(point[0]) > abc.index(other_point[0]):
+                                        if abc.index(point[0]) != 9:
+                                            likely_points.append(abc[abc.index(point[0]) + 1] + point[1])
+                                        if abc.index(other_point[0]) != 0:
+                                            likely_points.append(abc[abc.index(other_point[0]) - 1] + other_point[1])
+
+
+                clean_likely_points = []
+                for point in likely_points:
+                    if point in self.target_points:
+                        clean_likely_points.append(point)
+                for point in clean_likely_points:
+                    if clean_likely_points.count(point) > 1:
+                        while clean_likely_points.count(point) > 1:
+                            clean_likely_points.remove(point)
+                print("Clean likely points: ", clean_likely_points)
+                
+                hit_neighbors = self.enemy_grid.points_dict[self.hit_point].neighbors()
+                clean_hit_neighbors = []
+                for point in hit_neighbors:
+                    if point in self.target_points:
+                        clean_hit_neighbors.append(point)
+                print("clean hit neighbors", clean_hit_neighbors)
+
+                if len(clean_likely_points) > 0:
+                    target = random.choice(clean_likely_points)
+
+                elif len(clean_hit_neighbors) > 0:
+                    target = random.choice(clean_hit_neighbors)
+
+                else:
+                    self.hit_point = "  "
+            
+            return target   
+
+
+
+        if difficulty == 1:
+            target = random.choice(self.target_points)
+
+            if self.hit_point != "  ":
+
+                hit_neighbors = self.enemy_grid.points_dict[self.hit_point].neighbors()
+                clean_hit_neighbors = []
+                for point in hit_neighbors:
+                    if point in self.target_points:
+                        clean_hit_neighbors.append(point)
+                print(clean_hit_neighbors)
+                if len(clean_hit_neighbors) > 0:
+                    target = random.choice(clean_hit_neighbors)
+                else:
+                    self.hit_point = "  "
+            
+            return target   
+
+
+
+
+
+
 
 
     def fire(self, target):
         print("Firing at " + target)
-        if self.points_dict[target].is_ship == True:
+        self.target_points.remove(target)
+        self.previous_target = target
+        if self.enemy_grid.points_dict[target].is_ship == True:
             print("Hit!")
-            self.points_dict[target].hit()
-            self.print_grid()
+            self.hit_point = target
+            self.hit_list.append(target)
+            self.enemy_grid.points_dict[target].hit()
+            for ship in self.enemy_grid.all_ships:
+                if target in ship.position:
+                    ship.damage()
+                    self.enemy_grid.total_health += -1
+                    if ship.health == 0:
+                        ship.sink()
+
+            self.enemy_grid.print_grid()
         else:
             print("Miss!")
-            self.points_dict[target].miss()
-            self.print_grid()
+            self.enemy_grid.points_dict[target].miss()
+            self.enemy_grid.print_grid()
+
+    
+            
         
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
 
 grid1 = Grid("Your Grid:")
 grid2 = Grid("Enemy Grid:")
@@ -205,11 +383,31 @@ grid1.place_ship(Ship("Submarine", "Player", 3))
 grid1.place_ship(Ship("Patrol Boat", "Player", 2))
 grid1.place_ship(Ship("Battleship", "Player", 4))
 
-grid1.print_grid()
+grid2.place_ship(Ship("Destroyer", "Enemy", 3))
+grid2.place_ship(Ship("Carrier", "Enemy", 5))
+grid2.place_ship(Ship("Submarine", "Enemy", 3))
+grid2.place_ship(Ship("Patrol Boat", "Enemy", 2))
+grid2.place_ship(Ship("Battleship", "Enemy", 4))
 
-'''for x in range(0,3):
+
+
+
+Player1 = Player("Player", "Diego", grid1, grid2)
+Player2 = Player("Enemy", "Computer", grid2, grid1)
+
+
+
+grid1.print_grid()
+grid2.reveal_ships()
+
+
+'''for x in range(0,10):
     grid2.print_grid()
-    grid2.fire(input("Where would you like to fire? "))
+    Player1.fire(input("Where would you like to fire? "))
     grid1.print_grid()
-    grid1.fire(random.choice(grid1.points_list))'''
+    Player2.fire(random.choice(grid1.points_list))'''
     
+for x in range (0, 20):
+    Player1.fire(Player1.ai(2))
+
+
